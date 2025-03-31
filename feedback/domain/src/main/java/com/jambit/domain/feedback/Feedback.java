56e9bb5e727bd5a6c3f3c371cd8d3a809a5e6007
TigranMelkonyan <tigran.melqonyan96@gmail.com>
2025-03-31 16:15:50 +0400

    modif: clean up

--- feedback/domain/src/main/java/com/jambit/domain/feedback/Feedback.java
+++ feedback/domain/src/main/java/com/jambit/domain/feedback/Feedback.java
@@ -24,7 +24,7 @@ public class Feedback extends AuditableBaseEntity {
     private short score;
     private UUID userId;
 
-    //Defines business restrictions
+    //Validates business restrictions
     public static void validateContent(final Feedback feedback) {
         validateScore(feedback.getScore());
         validateComment(feedback.getComment());
